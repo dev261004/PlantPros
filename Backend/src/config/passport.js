c6d4810e41c88f86804ef757bfc7a9857d6edf6e
Dev Agrawal <agrawaldev261004@gmail.com>
2024-08-11 20:34:43 +0530

    authentication complete and some model are done

---
+++ Backend/src/config/passport.js
@@ -0,0 +1,20 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: process.env.GOOGLE_CALLBACK_URL
+},
+(accessToken, refreshToken, profile, done) => {
+  // Here you would look up or create the user in your database
+  return done(null, profile);
+}));
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
